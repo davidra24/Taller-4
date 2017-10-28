98a02db9314af7b404f468cf647e3180880e91d9
davidra24 <cdavidramirezg24@gmail.com>
2017-10-27 22:58:47 -0500

    Initial Commit

---
+++ build/web/js/Modelo.js
@@ -0,0 +1,34 @@
+var idFacultad=3;
+var facultades=[
+    {"id":1, "nombre":"Ingenieria"},
+    {"id":2, "nombre":"Licenciatura"}
+
+];
+var idCarrera=2;
+var carreras=[
+     {"id":1, "nombre":"Ingenieria de Sistemas y Computacion"},
+];
+
+var idMunicipio=4;
+var municipios=[
+    {"id":1, "nombre":"Tunja"},
+    {"id":2, "nombre":"Duitama"},
+    {"id":3, "nombre":"Sogamoso"},
+];
+
+var idProfesores=4;
+var profesores=[
+    {"id":1, "nombre":"Diego", "apellidos":"Torres", "documento":"1049651765"},
+    {"id":1, "nombre":"Alejandro", "apellidos":"Niño", "documento":"1049651766"},
+    {"id":1, "nombre":"Mariana", "apellidos":"Correa", "documento":"1049651767"},
+];
+
+var idMateria=2;
+var materias=[
+    {"id":1, "nombre":"Software II", "creditos":"4",},
+];
+
+var idEstudiante=2;
+var estudiantes=[
+    {"id":1, "nombre":"Diego", "apellido":"Torres", "codigo":"6265698", "documento":"77777777"},
+];
